1d50385e901bde7aeacf9f338985b8cda02ea133
Shivesh Kumar Pandey <urs.kumarshivesh@gmail.com>
2026-03-02 23:36:47 +0530

    Added auto-create for static folder

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ async def call_gemini(payload: dict, timeout: int = 30) -> dict:
 
 app = FastAPI(title="NEXUS AI Manager")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+os.makedirs("static", exist_ok=True)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 #templates = Jinja2Templates(directory="templates")
 templates = Jinja2Templates(directory="nexus-ai-manager-FINAL/nexus/templates")
